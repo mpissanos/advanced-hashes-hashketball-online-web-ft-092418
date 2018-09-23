7a9f49f2a73b7818e55274175e59bac7d56d563d
Manny Pissanos <mpissano@gmail.com>
2018-09-23 13:42:35 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -150,7 +150,7 @@ end
 def team_colors(team_name)
   game_hash.each do |location, info|
     if info.include?(team_name)
-      return game_hash[location][info][:colors].to_s
+      return game_hash[location][info][:colors]
     end
   end
 end
